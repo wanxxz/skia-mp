aa05f490dc0009dd8d28b6cc45d75e2b554bd1d4
Yiwan Zhou <mail@yiwan.me>
2025-12-21 08:33:46 +0000

    wip

--- public/pages/index.js
+++ public/pages/index.js
@@ -9,7 +9,6 @@ Component({
         .node()
         .exec(res => {
           const canvas = res[0].node
-          const ctx = canvas.getContext('webgl2')
 
           init(canvas).then(CanvasKit => {
             globalThis.CanvasKit = CanvasKit
